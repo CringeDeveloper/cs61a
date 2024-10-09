b38f9ee705dad66669c246fb3edc3c09f30ed0ca
Vladislav <warzon131@gmail.com>
2024-10-09 23:50:09 +0300

    tasks

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -14,6 +14,7 @@ def composite_identity(f, g):
     False
     """
     "*** YOUR CODE HERE ***"
+    return lambda x: f(g(x)) == g(f(x))
 
 
 def sum_digits(y):
@@ -60,6 +61,16 @@ def count_cond(condition):
     8
     """
     "*** YOUR CODE HERE ***"
+    def super_count(n):
+        i = 1
+        count = 0
+
+        while i <= n:
+            if condition(n, i):
+                count += 1
+            i += 1
+        return count
+    return super_count
 
 
 def multiple(a, b):
